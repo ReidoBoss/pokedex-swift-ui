49a55eec5794ab1362601e671bfb696eea7730c7
ReidoBoss <bryansagarino222@gmail.com>
2025-08-05 18:39:07 +0800

    chore: create app initialization

---
+++ Vaulkedex/Vaulkedex/VaulkedexApp.swift
@@ -0,0 +1,17 @@
+//
+//  VaulkedexApp.swift
+//  Vaulkedex
+//
+//  Created by Stephen T. Sagarino Jr. on 8/5/25.
+//
+
+import SwiftUI
+
+@main
+struct VaulkedexApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
